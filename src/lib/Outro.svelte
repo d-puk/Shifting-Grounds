649d7fe68ba4793533df488c7cd8d86221875972
Deepak Ramarao <deepakramarao77@gmail.com>
2025-03-22 12:36:24 +0530

    fixed footer

--- src/lib/Outro.svelte
+++ src/lib/Outro.svelte
@@ -12,7 +12,6 @@
         display: flex;
         padding: var(--spacing-2, 1rem) 0rem;
         justify-content: space-between;
-        align-self: stretch;
         width: 95%;
 
         border-block: 1px solid var(--border-strong, #B0AFBA);
